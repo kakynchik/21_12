581b2904aa5ceadbf58e46a26427e2aa23836ae1
kakynchik <ukakyn@gmail.com>
2025-04-15 20:41:56 +0300

    refactor tests and main program to count multiples of seven

--- 21_12/Program.cs
+++ 21_12/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace _21_12
+﻿namespace _21._12
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 14, 21, 35, 40, 49, 50, 63, 70, 100 };
             
+            Func<int[], int> countMultiplesOfSeven = arr => arr.Count(num => num % 7 == 0);
+            
+            int count = countMultiplesOfSeven(numbers);
+
+            Console.WriteLine($"Кількість чисел, кратних семи: {count}");
         }
     }
 }
--- Tests/ConsoleTests.cs
+++ Tests/ConsoleTests.cs
@@ -1,11 +1,20 @@
-﻿namespace Tests;
+﻿namespace TestProject1;
 
 [TestClass]
-public sealed class ConsoleTests
+public class UnitTest1
 {
+
     [TestMethod]
     public void TestMethod1()
     {
-    
+        // Arrange
+        int[] numbers = { 14, 21, 35, 40, 49, 50, 63, 70, 100 };
+        Func<int[], int> countMultiplesOfSeven = arr => arr.Count(num => num % 7 == 0);
+
+        // Act
+        int count = countMultiplesOfSeven(numbers);
+
+        // Assert
+        Assert.AreEqual(6, count, "The count of numbers divisible by 7 is incorrect.");
     }
 }
